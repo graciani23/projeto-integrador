361017c5f010d68be18cea3f8998cc822862a26d
stevapsys <stephanievapsys@gmail.com>
2020-01-18 21:26:19 -0300

    Criando uma nova home em php. Acrescentei os includes menu.php. header.php e cadastre.php

--- assets/includes/header.php
+++ assets/includes/header.php
@@ -1,19 +1,29 @@
-<nav>
-    <div class="container">
-        <ul>
-            <li><a href="sobre-nos.html">Sobre nós</a></li>
-            <li><a href="cad-log-usuario.html">Área do Usuário</a></li>
-            <li><a href="cad-log-empresa.html">Área da Empresa</a></li>
-            <li><a href="#">Contatos</a></li>
-        </ul>
-        <div class="img-cabecalho">
-            <img src="./assets/img/Brazil.png" alt="Bandeira do Brasil">
-            <img src="./assets/img/England.png" alt="Bandeira da Inglaterra">
-            <img src="./assets/img/France.png" alt="Bandeira da França">
-            <img src="./assets/img/Spain.png" alt="Bandeira da Espanha">
+<!DOCTYPE html>
+<html lang="pt">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Header</title>
+    <link rel="stylesheet" href="./assets/css/estiloheader.css">
+    <link href="https://fonts.googleapis.com/css?family=Montserrat&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Roboto+Slab&display=swap" rel="stylesheet">
+
+
+
+</head>
+<body>
+    <div class="fundo">
+        <h1>NOME DO SITE</h1>  
+        <h5>Uma frase de impacto para o site. Um slogan ou algo do tipo</h5> 
+        
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum, distinctio minima voluptas quisquam et nisi dolorem rem voluptatum, autem inventore dolores mollitia molestiae quod nihil voluptatem accusantium? Natus, facilis doloribus!
+            Officia nostrum, esse debitis neque ut nulla voluptatum. Voluptatibus cumque quo culpa illum ea repellendus in. Dicta iusto quisquam numquam quos sapiente, sequi ullam quidem impedit porro corrupti fugit obcaecati.
+            Culpa molestiae quidem quae hic non officiis, nesciunt quis expedita excepturi illum nemo deserunt perferendis nobis sunt earum quos. Deleniti obcaecati sequi illum sit dignissimos eum minus similique itaque? Repellendus?
+            Ratione quaerat inventore minima, doloremque non unde dolore voluptatibus molestias voluptas laboriosam minus quia rerum odit! Iusto earum quos nobis delectus ad, ab recusandae, quod repudiandae architecto dignissimos vel corporis?
+            Excepturi cumque cum dignissimos consequatur perspiciatis impedit, iste id corrupti ipsum. Eius, vero, natus odio repellendus vel omnis dolor assumenda quo architecto nam quia eligendi officia mollitia illum quaerat id!</p>
+
         </div>
-    </div>
-    <header>
-        <div class="container"></div>
-    </header>
-</nav>
+
+</body>
+</html>
--- sobre-nos.php
+++ sobre-nos.php
@@ -9,6 +9,12 @@
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
         integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
 </head>
+    <?php
+        require('./assets/includes/menu.php');
+        require('./assets/includes/header.php');
+        require('./assets/includes/cadastre.php');
+
+    ?>
 <body>
 
 </body>
