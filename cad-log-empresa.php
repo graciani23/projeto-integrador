d37c9d50b5ff0eec417f293266e1cdbcd4a55003
graciani23 <gracianigss@gmail.com>
2020-01-23 11:06:16 -0300

    alterações cad usuario e empresa

--- cad-log-empresa.php
+++ cad-log-empresa.php
@@ -5,95 +5,28 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link href="https://fonts.googleapis.com/css?family=Montserrat&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
-        integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-    <link rel="stylesheet" href="./assets/css/estiloFormsCadastro.css">
     <title>Cadastro e Login - Empresa</title>
 </head>
 <body>
     <?php
         require('./assets/includes/menu.php');
     ?>
-    <main class="container">
-        <section class="login">
-            <form>
-            <h3>Faça seu login</h3>
-                <div class="form-group">
-                    <label for="exampleInputEmail1">Endereço de email</label>
-                    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Seu email">
-                </div>
-                <div class="form-group">
-                    <label for="exampleInputPassword1">Senha</label>
-                    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Senha">
-                </div>
-                <div class="form-group form-check">
-                    <input type="checkbox" class="form-check-input" id="exampleCheck1">
-                    <label class="form-check-label" for="exampleCheck1">Esqueci minha senha</label>
-                </div>
-                <button type="submit" class="btn btn-light">Enviar</button>
-            </form>
-        </section>
-        <section class="cadastro">
-            <form>
-            <h3>Faça seu cadastro</h3>
-                <div class="form-row">
-                    <div class="form-group col-md-6">
-                        <label for="inputEmail4">Email</label>
-                        <input type="email" class="form-control" id="inputEmail4" placeholder="Email">
-                    </div>
-                    <div class="form-group col-md-6">
-                        <label for="inputPassword4">Senha</label>
-                        <input type="password" class="form-control" id="inputPassword4" placeholder="Senha">
-                    </div>
-                </div>
-                <div class="form-group">
-                    <label for="inputAddress">Endereço</label>
-                    <input type="text" class="form-control" id="inputAddress" placeholder="Rua dos Bobos, nº 0">
-                </div>
-                <div class="form-group">
-                    <label for="inputAddress2">Endereço 2</label>
-                    <input type="text" class="form-control" id="inputAddress2" placeholder="Apartamento, hotel, casa, etc.">
-                </div>
-                <div class="form-row">
-                    <div class="form-group col-md-6">
-                        <label for="inputCity">Cidade</label>
-                        <input type="text" class="form-control" id="inputCity">
-                    </div>
-                    <div class="form-group col-md-4">
-                        <label for="inputEstado">Estado</label>
-                        <select id="inputEstado" class="form-control">
-                            <option selected>Escolher...</option>
-                            <option>...</option>
-                        </select>
-                    </div>
-                    <div class="form-group col-md-2">
-                        <label for="inputCEP">CEP</label>
-                        <input type="text" class="form-control" id="inputCEP">
-                    </div>
-                </div>
-                <div class="form-group">
-                    <div class="form-check">
-                        <input class="form-check-input" type="checkbox" id="gridCheck">
-                        <label class="form-check-label" for="gridCheck">
-                            Clique em mim
-                        </label>
-                    </div>
-                </div>
-                <button type="submit" class="btn btn-light">Entrar</button>
-            </form>
-        </section>
-    </main>
+    <div class="corpo-form">
+        <form method="POST">
+            <p>Cadastre sua empresa</p>
+            <input type="text" id="inputName" placeholder="Nome da empresa">
+            <input type="number" id="inputCNPJ" placeholder="Informe seu CNPJ">        
+            <input type="email" id="inputEmail" placeholder="Email de acesso">
+            <input type="password" id="inputPassword" placeholder="Senha">
+            <input type="password" id="inputConfirmPassword" placeholder="Confirme sua senha">
+    
+            <button type="submit" class="btn btn-light">Entrar</button>
+        </form>
+    </div>
     <?php
         require('./assets/includes/footer.php');
     ?>
-    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js"
-        integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n"
-        crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"
-        integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo"
-        crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"
-        integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6"
-        crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="assets/css/estiloCadUsuarioEmpresa.css">
+    <link rel="stylesheet" href="assets/css/estiloFooter.css">
 </body>
 </html>
