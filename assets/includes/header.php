224d36f886d1c57b785e0ce389789f30d4406220
stevapsys <stephanievapsys@gmail.com>
2020-01-23 21:42:46 -0300

    Alterando menu e header do sobre nós

--- assets/includes/header.php
+++ assets/includes/header.php
@@ -6,24 +6,18 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Header</title>
     <link rel="stylesheet" href="./assets/css/estiloheader.css">
-    <link href="https://fonts.googleapis.com/css?family=Montserrat&display=swap" rel="stylesheet">
-    <link href="https://fonts.googleapis.com/css?family=Roboto+Slab&display=swap" rel="stylesheet">
-
-
+    <link href="https://fonts.googleapis.com/css?family=Montserrat|Roboto+Slab&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro&display=swap" rel="stylesheet">
 
 </head>
 <body>
-    <div class="fundo">
-        <h1>NOME DO SITE</h1>  
-        <h5>Uma frase de impacto para o site. Um slogan ou algo do tipo</h5> 
-        
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum, distinctio minima voluptas quisquam et nisi dolorem rem voluptatum, autem inventore dolores mollitia molestiae quod nihil voluptatem accusantium? Natus, facilis doloribus!
-            Officia nostrum, esse debitis neque ut nulla voluptatum. Voluptatibus cumque quo culpa illum ea repellendus in. Dicta iusto quisquam numquam quos sapiente, sequi ullam quidem impedit porro corrupti fugit obcaecati.
-            Culpa molestiae quidem quae hic non officiis, nesciunt quis expedita excepturi illum nemo deserunt perferendis nobis sunt earum quos. Deleniti obcaecati sequi illum sit dignissimos eum minus similique itaque? Repellendus?
-            Ratione quaerat inventore minima, doloremque non unde dolore voluptatibus molestias voluptas laboriosam minus quia rerum odit! Iusto earum quos nobis delectus ad, ab recusandae, quod repudiandae architecto dignissimos vel corporis?
-            Excepturi cumque cum dignissimos consequatur perspiciatis impedit, iste id corrupti ipsum. Eius, vero, natus odio repellendus vel omnis dolor assumenda quo architecto nam quia eligendi officia mollitia illum quaerat id!</p>
-
+    <div class="fundo">        
+            <h2 class="h2">Recomeço além das fronteiras</h2>
+            <button class="botao"> Participe  </button>
+            <br> <br> <br>
         </div>
-
+    <div class="fundo">
+            
+    </div>
 </body>
 </html>
--- assets/includes/menu.php
+++ assets/includes/menu.php
@@ -16,10 +16,10 @@
             <li><a href="sobre-nos.php">Sobre nós</a></li>
             <li><a href="cad-log-usuario.php">Área do Usuário</a></li>
             <li><a href="cad-log-empresa.php">Área da Empresa</a></li>
-            <li><a href="#">Contatos</a></li>
+            <li><a href="#">Contato</a></li>
         </ul>
         <div class="img-cabecalho">
-            <p style="color:white">Selecione o seu idioma</p>
+            <p style="color:white;margin-bottom:0rem;font-size:14px">Selecione o seu idioma</p>
             <img src="./assets/img/Brazil.png" alt="Bandeira do Brasil">
             <img src="./assets/img/England.png" alt="Bandeira da Inglaterra">
             <img src="./assets/img/France.png" alt="Bandeira da França">
