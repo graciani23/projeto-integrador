5fa5f4fb7f3306d68172927d8c069e28b654930e
Jeferson Silva <jeferson_1703@hotmail.com>
2020-02-05 08:55:09 -0300

    [Search] - tela de busca de vagas

---
+++ search.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <title>Search - Job</title>
+    <meta name="author" content="Jeferson Silva">
+
+    <!-- Link com o caminho da fonte utilizada no LOGO -->
+    <link href="https://fonts.googleapis.com/css?family=Montserrat&display=swap" rel="stylesheet">
+
+    <!-- Reset de todos os estilos CSS -->
+    <link rel="stylesheet" href="assets/css/reset.css">
+    <link rel="stylesheet" href="assets/css/search.css">
+</head>
+<body>
+    <header>
+        <div class="principal">
+
+        <!-- TAG PARA O LOGO PRINCIPAL DA PÁGINA -->
+        <div class="logo"><a href="sobre-nos.php">MigraJobs</a></div>
+
+        <nav>
+            <ul>
+                <li><a href="#">Minha área</a></li>
+                <li><a href="#">Candidaturas</a></li>
+                <li><a href="#">Buscar vagas</a></li>
+                <li><a href="#">Currículo</a></li>
+                <li><a href="#">Contato</a></li>
+            </ul>    
+        </nav>
+
+        <div class="user">
+            Olá, Jeferson
+        </div>
+
+        <img src="assets/icones/user.svg" alt="">
+        </div>
+    </header>
+
+    <section>
+    </section>
+
+    <!-- FOOTER -->
+    <footer>
+        <div class="logo">MigraJobs</div>
+        <br><br>© MigraJobs atividades de Internet, Ltda. Todos os direitos reservados.
+    </footer>
+</body>
+</html>
