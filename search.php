e0a9a993fa9681b762c66c391357c18b5a3888a4
Jeferson Silva <jeferson_1703@hotmail.com>
2020-02-06 18:46:35 -0300

    [search] - Finalizando área do usuário

--- search.php
+++ search.php
@@ -1,49 +1,111 @@
 <!DOCTYPE html>
 <html lang="pt-br">
+
 <head>
-    <meta charset="UTF-8">
     <title>Search - Job</title>
     <meta name="author" content="Jeferson Silva">
 
-    <!-- Link com o caminho da fonte utilizada no LOGO -->
+    <!-- Link com as fontes utilizadas para construção do site -->
     <link href="https://fonts.googleapis.com/css?family=Montserrat&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Varela+Round&display=swap" rel="stylesheet">
 
     <!-- Reset de todos os estilos CSS -->
     <link rel="stylesheet" href="assets/css/reset.css">
     <link rel="stylesheet" href="assets/css/search.css">
 </head>
+
 <body>
-    <header>
-        <div class="principal">
+    <header id="cabecalho">
+        <div id="cabecalho-central">
+            <!-- MigraJobs logo no header de navegação -->
+            <div class="logo">
+                <a href="sobre-nos.php">MigraJobs</a>
+            </div>
+            <nav>
+                <ul>
+                    <li><a href="#">Minha área</a></li>
+                    <li><a href="#">Candidaturas</a></li>
+                    <li><a href="#">Buscar vagas</a></li>
+                    <li><a href="#">Currículo</a></li>
+                    <li><a href="#">Contato</a></li>
+                </ul>
+            </nav>
 
-        <!-- TAG PARA O LOGO PRINCIPAL DA PÁGINA -->
-        <div class="logo"><a href="sobre-nos.php">MigraJobs</a></div>
+            <div class="user">Olá, Jeferson</div>
 
-        <nav>
-            <ul>
-                <li><a href="#">Minha área</a></li>
-                <li><a href="#">Candidaturas</a></li>
-                <li><a href="#">Buscar vagas</a></li>
-                <li><a href="#">Currículo</a></li>
-                <li><a href="#">Contato</a></li>
-            </ul>    
-        </nav>
-
-        <div class="user">
-            Olá, Jeferson
-        </div>
-
-        <img src="assets/icones/user.svg" alt="">
+            <img src="assets/icones/user.svg" alt="">
         </div>
     </header>
 
-    <section>
-    </section>
+    <div class="column-left">
+        <div class="profile">
+            <div class="addPhoto">FOTO</div>
+            <div class="nameCandidate">Jeferson Silva</div>
+            <div class="liCargo">Analista de Suporte / Service Desk</div>
+            <div class="linkCV"><a href="#">Atualizar CV</a></div>
+            <div class="statusbargreen"><span>100%</span></div>
+        </div>
 
-    <!-- FOOTER -->
-    <footer>
-        <div class="logo">MigraJobs</div>
-        <br><br>© MigraJobs atividades de Internet, Ltda. Todos os direitos reservados.
-    </footer>
+        <div class="buscar-vagas">
+            <h2>Buscar vagas</h2>
+            <input type="text" placeholder="Digite cargo ou área profissional">
+        </div>
+    </div>
+
+    <div class="column-vagas">
+        <div class="filtrar-vagas">
+            <div class="filtrar-vagas-titulo">Fitrar Resultados</div>
+                
+            <div class="palavra-chave">Palavra-chave</div>
+
+            <div class="input-filtrar-vagas">
+                <input type="text" placeholder="'Gerente Comercial'...">
+            </div>
+            <button>Filtrar</button>
+            
+            <br><br><hr><br>
+
+            <div class="palavra-chave">Palavra-chave</div>
+            
+            <label class="container">
+                <input type="checkbox">São Paulo - SP [4.501] <br><br>
+            </label>
+            <label class="container">
+                <input type="checkbox">Rio de Janeiro - RJ [840] <br><br>
+            </label>
+            <label class="container">
+                <input type="checkbox">Bahia - BA [2.209] <br><br>
+            </label>
+            <label class="container">
+                <input type="checkbox">R. Grande do Sul - RS [1.021] <br><br>
+            </label>
+        </div>
+
+        <div class="vagas">
+            <div class="titulo"> Atendente de Chat</div>
+            <div class="subtitulo">Telemarketing - SAC</div>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse doloribus debitis ullam labore architecto assumenda unde. Commodi ea asperiores, quidem vel amet.
+                O Profissional Irá Executar Atividades Relacionadas A Vendas Dos Produtos Onde Seu Foco É Satisfazer As Necessidades Dos Clientes. Captação E Formação De Carteira De Futuros Clientes Para Os Projetos Pelo Telefone, Chat E Pessoal. Ter Habilidade Na Interpretação Das Informações Técnicas Referen ... <a href="#">Visualizar Vaga</a>
+            </p>
+        </div>
+
+        <div class="vagas">
+            <div class="titulo"> Atendente de Chat</div>
+            <div class="subtitulo">Telemarketing - SAC</div>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse doloribus debitis ullam labore architecto assumenda unde. Commodi ea asperiores, quidem vel amet.
+                O Profissional Irá Executar Atividades Relacionadas A Vendas Dos Produtos Onde Seu Foco É Satisfazer As Necessidades Dos Clientes. Captação E Formação De Carteira De Futuros Clientes Para Os Projetos Pelo Telefone, Chat E Pessoal. Ter Habilidade Na Interpretação Das Informações Técnicas Referen ... <a href="#">Visualizar Vaga</a>
+            </p>
+        </div>
+    </div>
+
+    <div class="rodape">
+        <footer>
+            <div class="logo">MigraJobs</div>
+            <br><br>© MigraJobs atividades de Internet, Ltda. Todos os direitos reservados.
+        </footer>
+    </div>
+
 </body>
 </html>
