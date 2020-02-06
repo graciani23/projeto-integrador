b3d8a8b36f5c847fb75a884b4b9d0fce71d85da1
graciani23 <gracianigss@gmail.com>
2020-02-06 21:52:02 -0200

    background telas de cadastro usuário e empresa

--- cad-log-usuario.php
+++ cad-log-usuario.php
@@ -5,36 +5,38 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link href="https://fonts.googleapis.com/css?family=Montserrat&display=swap" rel="stylesheet">
+    
+    <link rel="stylesheet" href="assets/css/estiloMenuNav.css">
+    <link rel="stylesheet" href="assets/css/estiloFooter.css">
+    <link rel="stylesheet" href="assets/css/estiloCadUsuarioEmpresa.css">
     <title>Login Usuário</title>  
 </head>
 <body>
     <?php
         require './assets/includes/menuNav.php';
     ?>
-    <div class="bg" style="background-image: url('assets/img/inclusao-social.png');">
-        <div class="corpo-form">
-            <form method="POST">
-                <h2><strong>Cadastre seu currículo e tenha acesso aos nossos serviços</strong></h2>
-                <input type="text" id="inputName" placeholder="Digite seu nome completo">
-                <div class="genero">
-                    <input type="radio" name="genero" id="masculino" value="option1" checked>
-                    <label for="masculino">Masculino</label>
-                    <input type="radio" name="genero" id="feminino" value="option1">
-                    <label for="feminino">Feminino</label>
-                </div>
-                <div>
-                    <label for="data">Data de nascimento</label>
-                    <input type="date" value="2011-08-19" id="data" placeholder="Data de nascimentos">
-                </div>
-                <input type="email" id="inputEmail" placeholder="Email">
-                <input type="password" id="inputPassword" placeholder="Senha">
-                <input type="password" id="inputConfirmPassword" placeholder="Confirme sua senha">
-                <?php
-                    require './assets/includes/comboBoxPaises.html'
-                ?>
-                <button type="submit" class="btn btn-light">Cadastrar</button>
-            </form>
-        </div>
+    <div class="corpo-form">
+        <form method="POST">
+            <h2><strong>Cadastre seu currículo e tenha acesso aos nossos serviços</strong></h2>
+            <input type="text" id="inputName" placeholder="Digite seu nome completo">
+            <div class="genero">
+                <input type="radio" name="genero" id="masculino" value="option1" checked>
+                <label for="masculino">Masculino</label>
+                <input type="radio" name="genero" id="feminino" value="option1">
+                <label for="feminino">Feminino</label>
+            </div>
+            <div>
+                <label class="labelForm" for="data">Data de nascimento</label>
+                <input type="date" value="2011-08-19" id="data" placeholder="Data de nascimentos">
+            </div>
+            <input type="email" id="inputEmail" placeholder="Email">
+            <input type="password" id="inputPassword" placeholder="Senha">
+            <input type="password" id="inputConfirmPassword" placeholder="Confirme sua senha">
+            <?php
+                require './assets/includes/comboBoxPaises.html'
+            ?>
+            <button type="submit" class="btn btn-light">Cadastrar</button>
+        </form>
     </div>
     
     <?php
@@ -42,8 +44,5 @@
     ?>
     
     
-    <link rel="stylesheet" href="assets/css/estiloMenuNav.css">
-    <link rel="stylesheet" href="assets/css/estiloFooter.css">
-    <link rel="stylesheet" href="assets/css/estiloCadUsuarioEmpresa.css">
 </body>
 </html>
