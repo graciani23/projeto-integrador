5f59824a480a6b571bb561d78c04743e366f29bb
Tatiana <tatitasrm@gmail.com>
2020-02-07 03:07:18 -0300

    Alteração do documento html e css e includes em PHP para inserção para a próxima sprint

---
+++ assets/includes/menuempresa.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Área da empresa</title>
+
+    <link href="https://fonts.googleapis.com/css?family=Montserrat|Roboto+Slab&display=swap" rel="stylesheet">
+
+    <link rel="stylesheet" href="./assets/css/estiloheaderempresa.css">
+
+</head>
+<body>
+<nav> 
+    <div class="container">
+        <div>
+        <ul>
+            <li><a href="minhaarea.php">Minha área</a></li>
+            <li><a href=".php">Área do Usuário</a></li>
+            <li><a href="#">Contato</a></li>
+        </ul>
+        </div>
+        <div class="img-cabecalho">
+            <p style="color:white;margin-bottom:0rem;font-size:14px">Selecione o seu idioma</p>
+            <img src="./assets/img/Brazil.png" alt="Bandeira do Brasil">
+            <img src="./assets/img/England.png" alt="Bandeira da Inglaterra">
+            <img src="./assets/img/France.png" alt="Bandeira da França">
+            <img src="./assets/img/Spain.png" alt="Bandeira da Espanha">
+        </div>
+    </div>
+</nav>
+</body>
+</html>
