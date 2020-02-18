1a4badbe6ea96a1abae19cd60576f9b61d8e32da
stevapsys <stephanievapsys@gmail.com>
2020-02-17 22:39:14 -0300

    mexendo na responsividade do index.php

--- index.php
+++ index.php
@@ -12,7 +12,7 @@
 </head>
 <body>
     <?php
-        require('./assets/includes/menu.php');        ?>
+        require('./assets/includes/menuResponsivo.php');        ?>
     <div class="fundo">        
         <h2 class="h2">Recomeço além das fronteiras</h2>
             <button class="botao"> 
@@ -25,17 +25,17 @@
     <section class="sobrenos">
         <div class="container">
             <div class="row">
-                <div class="col">
+                <div class="col col-lg-4 col-sm-12">
                     <h2 class="quem">QUEM</h2>
                     <h6 class="somos">SOMOS?</h6>
                      <p class="psobrenos"> A MigraJobs acredita na reinserção social e econômica de imigrantes e refugiados no Brasil. Mais que uma plataforma de empregos, visamos a inclusão social, econômica e cultural de quem imigrou para o Brasil</p>
                 </div>
-                <div class="col">
+                <div class="col col-lg-4 col-sm-12">
                     <h2 class="quem"> O QUE </h2>
                     <h6 class="somos">FAZEMOS?</h6>  
                     <p class="psobrenos">Nosso papel é servir como ponte entre imigrantes e refugiados que buscam trabalho digno e empresas que querem contratar. Nossa missão é promover o encontro de pessoas talentosas com empresas que precisam de gente em diversas áreas de atuação</p>
                 </div>
-                <div class="col">
+                <div class="col col-lg-4 col-sm-12">
                     <h2 class="quem"> QUEM </h2>
                     <h6 class="somos">NOS APOIA?</h6>  
                     <p class="psobrenos">Contamos com o suporte de ONGS como o CAMI (Centro de Apoio ao Imigrante) para realizar a triagem e cadastro de perfis e ao mesmo tempo buscamos parcerias de empresas que acredite nos nossos valores e nossa missão</p>
@@ -62,7 +62,7 @@
                             <a href="" class="senha"> Esqueci minha senha</a>
                             <a href="cad-log-usuario.php" class="senha">Quero me cadastrar</a>
                     <div>
-                        <button type="submit" class="btn btn-light">Entrar</button>
+                        <button type="submit" class="entrar">Entrar</button>
                         </div>
                     </div>
                 </div>      
@@ -85,7 +85,7 @@
                         <a href="http://" class="senha">Esqueci minha senha</a>
                         <a href="cad-log-empresa.php" class="senha">Quero me cadastrar</a>
                     <div>
-                        <button type="submit" class="btn btn-light">Entrar</button>
+                        <button type="submit" class="entrar">Entrar</button>
                     </div>
                 </div>
             </div>  
@@ -111,7 +111,7 @@
                         </div>
                         <br>
                     <div class="form-group form-check">
-                        <button type="submit" class="btn btn-light">Enviar</button>
+                        <button type="submit" class="entrar">Enviar</button>
                  </div>
             </div>  
         <br>
